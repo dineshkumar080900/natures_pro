fdbbefa4e492f3cf0f15646d65fe1e5f3d5c3b3f
Dinesh <dinesh>
2025-01-27 15:53:38 +0530

    save

--- BackEnd/Connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require("mongoose");
-const connect=mongoose.connect('mongodb+srv://dineshkumar:dinesh@cluster0.mysx0.mongodb.net/yourDatabaseName')
-
-.then(()=>{
-    console.log("Mongo DB Connected");
-}).catch((err)=>{
-    console.log(err.name);
-})
-module.exports=connect;
