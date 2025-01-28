7347640f845fa8aeb38dd5f119cfde285f8931d7
dineshkumar080900 <157379162+dineshkumar080900@users.noreply.github.com>
2025-01-28 12:26:39 +0530

    Update app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -93,8 +93,9 @@ function initializeApp() {
   setupErrorHandling();
 }
 
+
 // Start server
 initializeApp();
-
+const host ='0.0.0.0';
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port,host, () => console.log(`Server is running on port ${port}`));
